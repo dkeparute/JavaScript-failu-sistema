f9076335ed7a2eafa1747e825353cb61f8bbf69c
Dovilė <dovile.keparute@gmail.com>
2021-12-10 12:34:24 +0200

    Data library

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('labas');
+const data = require('../4-failu-sistema/library/data.js');
+
+console.log(data);
+
+data.create();
+data.read();
+data.update();
+data.delete();
--- library/data.js
+++ library/data.js
@@ -1,5 +1,23 @@
-// CRUD
+// FAILU SISTEMOS - CRUD
 // c - create
 // r - read
 // u -update
 // d- delete
+
+const data = {};
+
+data.create = (dir, fileName, content) => {
+    console.log('kuriamas failas');
+}
+data.read = (dir, fileName) => {
+    console.log('skaitomas failas');
+}
+data.update = (dir, fileName, content) => {
+    console.log('atnaujinamas failas');
+}
+data.delete = (dir, fileName) => {
+    console.log('trinamas failas');
+}
+
+
+module.exports = data;
